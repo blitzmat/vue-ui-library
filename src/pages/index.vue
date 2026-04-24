7812e41b9395a26c67209ce72f45f7b25295cf58
Hannes Matthysen <hannes.matthysen@vmlyr.com>
2026-04-24 12:10:06 +0200

    update copy

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,7 +7,7 @@ const { menuGroups } = useGroupedMenu()
 </script>
 
 <template>
-    <h1 class="h1">VMLY&R - UI Component Library</h1>
+    <h1 class="h1">Vue 3 UI Component Library</h1>
     <p>This is a front-end component ui library build in [<a href="https://vuejs.org/" target="_blank">Vue 3</a>] + [<a href="https://www.typescriptlang.org/" target="_blank">Typescript</a>] + [<a href="https://vitejs.dev/" target="_blank">Vite</a>] + [<a href="https://tailwindcss.com/" target="_blank">Tailwindcss</a>]</p>
     <p>Components included in this packages:</p>
     <template v-for="group in menuGroups" :key="group.key">
